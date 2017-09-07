3e210d3fefd851f3bbc93824fd286e7a06ec4b6b
Daniel Smilkov <dsmilkov@gmail.com>
2017-09-07 16:18:53 -0400

    update version and add publish-npm.sh script (#107)
    * update version and add publish-npm.sh script
    
    * run publish only if previous commands succeed
    
    * update readme

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2018 Google LLC. All Rights Reserved.
+# Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,34 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
-
-# Before you run this script, do this:
-# 1) Update the version in package.json
-# 2) Run ./scripts/make-version from the base dir of the project.
-# 3) Run `yarn` to update `yarn.lock`, in case you updated dependencies
-# 4) Commit to the master branch.
-
-# Then:
-# 5) Checkout the master branch of this repo.
-# 6) Run this script as `./scripts/publish-npm.sh` from the project base dir.
-
-set -e
-
-BRANCH=`git rev-parse --abbrev-ref HEAD`
-ORIGIN=`git config --get remote.origin.url`
-
-if [ "$BRANCH" != "master" ]; then
-  echo "Error: Switch to the master branch before publishing."
-  exit 1
-fi
-
-if ! [[ "$ORIGIN" =~ tensorflow/tfjs ]]; then
-  echo "Error: Switch to the main repo (tensorflow/tfjs) before publishing."
-  exit 1
-fi
-
-yarn build-npm
-./scripts/make-version # This is for safety in case you forgot to do 2).
-./scripts/tag-version
+npm run prep && \
+rm -rf dist/ && \
+node_modules/.bin/tsc && \
 npm publish
 echo 'Yay! Published a new package to npm.'
